ba37f185ef2d00be4b36bba35fea223a1d9b8ba2
muhanadmk <mouhandmk1@gmail.com>
2022-05-04 15:01:34 +0200

    add component facsnap dans l'app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from './modles/face-snap.modle';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'snapface';
+export class AppComponent implements OnInit{
+  mySnap!: FaceSnap;
+  mySnap2!: FaceSnap;
+  mySnap3!: FaceSnap;
+
+  ngOnInit(): void {
+    this.mySnap = new FaceSnap(
+    'Archibald',
+    'Mon meilleur ami depuis tout petit !',
+    'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+    new Date(),
+    0,
+    ) 
+    this.mySnap2 = new FaceSnap(
+      'Archibald',
+      'Mon meilleur ami depuis tout petit !',
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      new Date(),
+      0,
+      ) 
+  
+      this.mySnap3 = new FaceSnap(
+        'Archibald',
+        'Mon meilleur ami depuis tout petit !',
+        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        new Date(),
+        0,
+        ) 
+  }
 }
+
---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit , Input} from '@angular/core';
+import { FaceSnap } from '../modles/face-snap.modle';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent  implements OnInit{
+  @Input() faceSnap!: FaceSnap;
+  btnText!: string;
+  ngOnInit() {
+    this.btnText = 'Oh Snap';
+  }
+  
+  onSnap() {
+    if (this.btnText === 'Oh Snap!') {
+      this.faceSnap.snaps++;
+      this.btnText = 'Oops, unSnap!';
+    } else {
+      this.faceSnap.snaps--;
+      this.btnText = 'Oh Snap!'
+    }
+  }
+  
+}
